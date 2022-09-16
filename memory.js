6d50135c065fdc8fa2f5d210ace550058dd707ae
“Housam <housam.kak20@gmail.com>
2022-09-16 19:10:39 +0300

    Added image class initiated the randomize function

--- memory.js
+++ memory.js
@@ -1,2 +1,12 @@
 lifePoint = document.querySelector("#life");
 cards = document.querySelectorAll(".card");
+
+
+function RandomizeImages{
+    "/images/HTML-1.png"
+    "/images/HTML-2.png"
+    "/images/JAVA-1.jpg"
+    "/images/JAVA-2.jpg"
+    "/images/HTML-1.png"
+    "/images/HTML-1.png"
+}
